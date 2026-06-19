a300048e9be5c8691be5d4f4d4eaba35163c4059
dwaynebroussard67-art <dwaynebroussard67@gmail.com>
2026-06-19 15:07:30 -0500

    Update AuthContext.tsx

--- src/lib/AuthContext.tsx
+++ src/lib/AuthContext.tsx
@@ -29,7 +29,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
 
   return (
     <AuthContext.Provider value={{ user, signOut }}>
-      {!loading && children}
+      {children}
     </AuthContext.Provider>
   );
 }
